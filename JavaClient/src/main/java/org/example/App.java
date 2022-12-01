3e93f18274645d6973ec97324a225be708d6da0b
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2022-12-01 22:02:04 +0100

    piuuusb

--- JavaClient/src/main/java/org/example/App.java
+++ JavaClient/src/main/java/org/example/App.java
@@ -5,18 +5,22 @@ import com.soap.ws.client.generated.GeoLoca;
 import com.soap.ws.client.generated.IBikeService;
 import com.soap.ws.client.generated.Itinary;
 
+import javax.jms.JMSException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println("Hello World! we are going to test a SOAP client written in Java");
+    public static void main( String[] args ) throws JMSException {
+        System.out.println("---Welcome on Let's Biking app!---");
         Bike bike = new Bike();
         IBikeService bikeService= bike.getBasicHttpBindingIBikeService();
         Itinary itinary = bikeService.getItinerary("150 Rue Saint-Sever, 76100 Rouen","139 Rue du Gros Horloge, 76000 Rouen");
+
+        Consumer consumer = new Consumer();
         System.out.println(itinary.getFeatures().getValue().getFeatureItinary().get(0).getProperties().getValue().getSegments().getValue().getSegment().get(0).getSteps().getValue().getStep().get(0).getInstruction().getValue());
+        System.out.println(consumer.receiveMessage());
     }
 }
---
+++ JavaClient/src/main/java/org/example/Consumer.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+public class Consumer {
+    int TIMEOUT = 1;
+    Connection connection;
+    ConnectionFactory factory;
+    Destination destination;
+    String DESTINATION_NAME = "Hello.q";
+    Session session;
+    MessageConsumer messageConsumer;
+
+    public Consumer() throws JMSException {
+        factory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+        connection = factory.createConnection();
+        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        destination = session.createQueue(DESTINATION_NAME);
+    }
+
+    public void startConsumer() throws JMSException {
+        connection.start();
+        messageConsumer = session.createConsumer(destination);
+    }
+
+    public String receiveMessage() throws JMSException {
+        Message message = messageConsumer.receive(TIMEOUT);
+        String text = "";
+        if (message != null)
+            text = ((TextMessage) message).getText();
+        return text;
+    }
+}
