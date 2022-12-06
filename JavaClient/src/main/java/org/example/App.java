48a659946c8503889bc5d3130fb99d7e28a28485
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2022-12-06 19:59:00 +0100

    oi^hgiouhylvljuh vmipkujv

--- JavaClient/src/main/java/org/example/App.java
+++ JavaClient/src/main/java/org/example/App.java
@@ -3,6 +3,7 @@ package org.example;
 import POJO.ItinaryJava;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.soap.ws.client.generated.*;
+import org.example.Map.UserGUI;
 
 import javax.jms.JMSException;
 import java.io.*;
@@ -17,12 +18,12 @@ public class App {
     public static void main(String[] args) throws Exception, Exception {
         System.out.println("---Welcome on Let's Biking app!---");
 
-        Client client = new Client();
-        client.getItinaryByQueue();
+        /*Client client = new Client(new UserGUI());
+        client.getItinaryByQueue("Livraison Par Le, 20 Rue de l'Amitié, Bd Président John Fitzgerald Kennedy, 25000 Besançon", "91-93 Bd Léon Blum, 25000 Besançon", "");
         //System.out.println(client.readQueue());
         client.update();
         System.out.println("After update");
-       // System.out.println(client.readQueue());
+       // System.out.println(client.readQueue());*/
 
     }
 
